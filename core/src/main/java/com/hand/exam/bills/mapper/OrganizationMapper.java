9bd58c20df58ee330b5836ecc13a2510b3cb4285
xu <jian.xu04@hand-china.com>
2019-01-07 11:38:14 +0800

    配置仓库lov

--- core/src/main/java/com/hand/exam/bills/mapper/OrganizationMapper.java
+++ core/src/main/java/com/hand/exam/bills/mapper/OrganizationMapper.java
@@ -3,6 +3,11 @@ package com.hand.exam.bills.mapper;
 import com.hand.hap.mybatis.common.Mapper;
 import com.hand.exam.bills.dto.Organization;
 
-public interface OrganizationMapper extends Mapper<Organization>{
+import java.util.List;
+import java.util.Map;
 
+public interface OrganizationMapper extends Mapper<Organization>{
+    List<Map<String,Object>> selectOrganizationInfo(Map<String,Object> map);
+
+    List<Map<String,Object>> selectToOrganizationInfo(Map<String,Object> map);
 }
