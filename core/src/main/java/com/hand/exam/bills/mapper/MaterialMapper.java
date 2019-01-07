8a6c57c801602d1dd7ac67c2271dd7737f69e094
dzy <448372734@qq.com>
2019-01-07 10:36:00 +0800

    新增物料编码的LOV

--- core/src/main/java/com/hand/exam/bills/mapper/MaterialMapper.java
+++ core/src/main/java/com/hand/exam/bills/mapper/MaterialMapper.java
@@ -3,6 +3,14 @@ package com.hand.exam.bills.mapper;
 import com.hand.hap.mybatis.common.Mapper;
 import com.hand.exam.bills.dto.Material;
 
+import java.util.List;
+
 public interface MaterialMapper extends Mapper<Material>{
 
+    /**
+     * Lov查询
+     * @param condition
+     * @return
+     */
+    List<Material> selectLovMaterial(Material condition);
 }
